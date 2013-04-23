2d1c6d0092b26ef25e3627bb5647f08bd9b6d73c
shuimenyuan <joebo@yoho.cn>
2013-04-23 16:32:25 +0800

    add logic test

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by huan on 13-4-23.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
